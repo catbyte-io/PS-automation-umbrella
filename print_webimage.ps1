18d713a8f823a11ca4817da6d22f733e52a66978
Catbyte-io <74119748+catbyte-io@users.noreply.github.com>
2022-11-02 15:14:09 -0600

    Save All Website Images
    Saves all images from a given website to a folder of the user's choice

---
+++ print_webimage.ps1
@@ -0,0 +1,43 @@
+#function definition for select folder dialogue
+function Get-Folder($initialDirectory="")
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
+
+    $folderdialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    $folderdialog.Description = "Select folder"
+    $folderdialog.RootFolder = "Desktop"
+    $folderdialog.SelectedPath = "C:\Users\Default\Downloads\"
+
+    if($folderdialog.ShowDialog() -eq "OK")
+    {
+        $folder += $folderdialog.SelectedPath
+    }
+    elseif($folderdialog.ShowDialog() -eq "Cancel")
+    {
+        $folder += $folderdialog.RootFolder
+    }
+    return $folder
+}
+
+#Get Location and number of folders to be made by the user, then make that many folders numbered with a padding of two characters
+$saveto = Read-Host -Prompt "Would you like to specify a parent directory? y/n"
+if ($saveto -eq "y" -or $saveto -eq "yes")
+{    
+    $Location = Get-Folder
+}
+else 
+{
+    $Location = "C:\Users\Public\Downloads"
+    Write-Host "Default download folder will be used"
+}
+
+Write-Host $Location
+
+$URL = Read-Host "Enter or copy/paste the url"
+
+$WebResponse = Invoke-WebRequest $URL
+ForEach ($Image in $WebResponse.Images)
+{
+    $FileName = Split-Path $Image.src -Leaf
+    Invoke-WebRequest $Image.src -OutFile $FileName
+}
